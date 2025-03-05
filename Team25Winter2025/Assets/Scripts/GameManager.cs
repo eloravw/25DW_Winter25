a63a66910d2b9da2232f547e21da653927057392
eloravw <67245265+eloravw@users.noreply.github.com>
2025-03-05 17:14:52 -0500

    Updated game manager to reflect puzzle plans

--- Team25Winter2025/Assets/Scripts/GameManager.cs
+++ Team25Winter2025/Assets/Scripts/GameManager.cs
@@ -4,8 +4,54 @@ using UnityEngine;
 
 public static class GameManager
 {
-
+    //Ouiji Board Puzzle Pieces
     public static bool ouijaLampCollected { get; set; }
     public static bool ouijaCloakCollected { get; set; }
     public static bool ouijaCandleCollected { get; set; }
+
+    //Clues
+
+    //Big Translation Clue
+    public static bool TranslationClue { get; set; }
+
+    //Seance Room 1 Clues
+    public static bool clueC { get; set; }
+    public static bool clueF { get; set; }
+
+    //Seance Room 2 Clues
+    public static bool clueO { get; set; }
+    public static bool clueX { get; set; }
+
+    //Music Room Clues
+    public static bool clueK { get; set; }
+    public static bool clueL { get; set; }
+
+    //Entrance
+    public static bool clueM { get; set; }
+    public static bool clueP { get; set; }
+
+    //Library
+    public static bool clueN { get; set; }
+    public static bool clueR { get; set; }
+
+    //Yard
+    public static bool clueV { get; set; }
+    public static bool clueS { get; set; }
+
+    //Beach
+    public static bool clueI { get; set; }
+    public static bool clueA { get; set; }
+    public static bool clueJ { get; set; }
+
+    //Cave
+    public static bool clueZ { get; set; }
+    public static bool clueT { get; set; }
+    public static bool clueD { get; set; }
+
+    //Mountain
+    public static bool clueE { get; set; }
+    public static bool clueB { get; set; }
+
+
+
 }
